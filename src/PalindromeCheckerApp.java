20904f68f6c25395d13afdca1c05a01f77da29b6
utkxrshrxj <rajutkarsh.3004@gmail.com>
2026-02-25 09:28:51 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("==============================");
+        System.out.println("WELCOME");
+        System.out.println("==============================");
+        System.out.println("This Application checks whether a word is a palindrome or not");
+        System.out.println("Stay Tuned for upcoming features!!!");
+    }
+}
