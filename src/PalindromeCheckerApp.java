72e45a210811ae702b32e458a15013e3f4d4311a
utkxrshrxj <rajutkarsh.3004@gmail.com>
2026-02-25 11:22:19 +0530

    UC6:Stack+Queue based Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class PalindromeCheckerApp {
+public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=====================================");
-        System.out.println("     PALINDROME CHECKER        ");
+        System.out.println("     PALINDROME CHECKER - UC6        ");
         System.out.println("=====================================");
         System.out.println("This Application checks whether a word is a palindrome or not");
         System.out.print("Enter a word: ");
@@ -16,16 +18,17 @@ public class PalindromeCheckerApp {
         String word = scanner.nextLine();
 
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         for (int i = 0; i < word.length(); i++) {
             stack.push(word.charAt(i));
+            queue.add(word.charAt(i));
         }
 
         boolean isPalindrome = true;
 
-        for (int i = 0; i < word.length(); i++) {
-            char poppedChar = stack.pop();
-            if (word.charAt(i) != poppedChar) {
+        while (!stack.isEmpty()) {
+            if (!stack.pop().equals(queue.remove())) {
                 isPalindrome = false;
                 break;
             }
