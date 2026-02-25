6c066f6cc36a4f18ad8f655cfa5e65339682d3a9
utkxrshrxj <rajutkarsh.3004@gmail.com>
2026-02-25 11:25:15 +0530

    UC7:Dequeue Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,34 +1,31 @@
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
-public class UseCase6PalindromeCheckerApp {
+public class UseCase7PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=====================================");
-        System.out.println("     PALINDROME CHECKER - UC6        ");
+        System.out.println("     PALINDROME CHECKER - UC7        ");
         System.out.println("=====================================");
         System.out.println("This Application checks whether a word is a palindrome or not");
         System.out.print("Enter a word: ");
 
         String word = scanner.nextLine();
 
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
+        Deque<Character> deque = new ArrayDeque<>();
 
         for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
-            queue.add(word.charAt(i));
+            deque.addLast(word.charAt(i));
         }
 
         boolean isPalindrome = true;
 
-        while (!stack.isEmpty()) {
-            if (!stack.pop().equals(queue.remove())) {
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
                 isPalindrome = false;
                 break;
             }
