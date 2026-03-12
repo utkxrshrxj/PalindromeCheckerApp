c9fbd9d4ad32ee862f3dc8046efea77e3301d9a8
utkxrshrxj <rajutkarsh.3004@gmail.com>
2026-03-12 09:09:30 +0530

    UC9: Recursive Palindrome Checker

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Palindrome Checker");
+
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        if (word.equalsIgnoreCase(reverse)) {
+            System.out.println("It is a Palindrome");
+        } else {
+            System.out.println("It is NOT a Palindrome");
+        }
+
+        sc.close();
+    }
+}
